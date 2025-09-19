aa1a6e6f5897d28afa15cdd035794c782fd163c9
Artem <la.artem.ser@gmail.com>
2025-09-19 13:20:52 +0300

    implement сentered elements on the card

--- Apps/HelloApp.cs
+++ Apps/HelloApp.cs
@@ -5,15 +5,17 @@ public class HelloApp : ViewBase
 {
     public override object? Build()
     {
-        var nameState = this.UseState<string>();
+        var nameInput = this.UseState<string>();
+        var displayName = this.UseState<string>("");
 
         return Layout.Center()
                | (new Card(
-                   Layout.Vertical().Gap(6).Padding(2)
+                    Layout.Vertical().Gap(6).Padding(2).Align(Align.Center)
                    | new Confetti(new IvyLogo())
-                   | Text.H2("Hello " + (string.IsNullOrEmpty(nameState.Value) ? "there" : nameState.Value) + "!")
+                   | Text.H2("Hello " + (string.IsNullOrEmpty(displayName.Value) ? "there" : displayName.Value) + "!")
                    | Text.Block("Welcome to the fantastic world of Ivy. Let's build something amazing together!")
-                   | nameState.ToInput(placeholder: "What is your name?")
+                   | nameInput.ToInput(placeholder: "What is your name?")
+                   | new Button("Say Hello!", onClick: _ => displayName.Value = nameInput.Value)
                    | new Separator()
                    | Text.Markdown("You'd be a hero to us if you could ⭐ us on [Github](https://github.com/Ivy-Interactive/Ivy-Framework)")
                  )
